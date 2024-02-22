7f504bd109b59433f1f41ed7ee6330e9e760ea14
Chris Padwick <cgpadwick@gmail.com>
2024-02-21 19:11:08 -0800

    add some cmd line options

--- generate_captions.py
+++ generate_captions.py
@@ -5,14 +5,13 @@ import os
 from matplotlib import pyplot as plt
 
 from pathlib import Path
-
 from PIL import Image
-
+from tqdm import tqdm
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
 
 
-def caption_images(image_dir, force_cpu=False):
+def caption_images(image_dir, output_dir, force_cpu=False, showimages=False):
     """
     Captions images in the specified image directory.
 
@@ -41,7 +40,7 @@ def caption_images(image_dir, force_cpu=False):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    for image_file in os.listdir(image_dir):
+    for image_file in tqdm(os.listdir(image_dir)):
         image = Image.open(image_dir / Path(image_file))
         if image.mode != "RGB":
             image = image.convert(mode="RGB")
@@ -58,7 +57,10 @@ def caption_images(image_dir, force_cpu=False):
 
         plt.imshow(image)
         plt.title(preds[0])
-        plt.show()
+        if showimages:
+            plt.show()
+
+        plt.savefig(output_dir / Path(image_file))
         del pixel_values
 
 
@@ -71,11 +73,24 @@ if __name__ == "__main__":
         help="location where the images will be read from",
     )
     parser.add_argument(
+        "--outputdir",
+        required=True,
+        type=Path,
+        help="location where the images and captions will be saved",
+    )
+    parser.add_argument(
         "--forcecpu",
         required=False,
         type=int,
         default=None,
         help="force the predicition to occur on the CPU",
+    )
+    parser.add_argument(
+        "--show",
+        required=False,
+        type=int,
+        default=1,
+        help="show the image with the generated caption in a window",
     )
 
     args = parser.parse_args()
@@ -83,4 +98,12 @@ if __name__ == "__main__":
     if not args.imagedir.exists():
         raise ValueError(f"Invalid path specified: {args.imagedir}")
 
-    caption_images(args.imagedir, force_cpu=args.forcecpu)
+    if not args.outputdir.exists():
+        raise ValueError(f"Invalid path specified: {args.outputdir}")
+
+    caption_images(
+        args.imagedir,
+        force_cpu=args.forcecpu,
+        showimages=args.show,
+        output_dir=args.outputdir,
+    )
