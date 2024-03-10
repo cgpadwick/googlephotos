f39294134a0fa8de4d720efb5aa2f8498426a06b
Chris Padwick <cgpadwick@gmail.com>
2024-03-10 15:40:04 -0700

    add cloud function

---
+++ cloud_functions/pubsub_trigger/main.py
@@ -0,0 +1,23 @@
+import base64
+from google.cloud import pubsub_v1
+
+
+def hello_pubsub(event, context):
+    """
+    A function to handle a Pub/Sub event. It decodes the message from the event, sets up the Pub/Sub publisher client, and publishes the message to a specified topic.
+    Parameters:
+        event: dict, The Pub/Sub event data.
+        context: google.cloud.functions.Context, The event metadata.
+    Returns:
+        None
+    """
+
+    message = base64.b64decode(event["data"]).decode("utf-8")
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_name = f"projects/cgp-project/topics/cgp-processed-topic"
+    msg = f"Received message: {message}"
+    data = msg.encode("utf-8")
+    future = publisher.publish(topic_name, data=data)
+    future.result()
+    print(msg)
