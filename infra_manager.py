26fa4c24cfaf54c5ae1b1f217048f17a029b61b7
Chris Padwick <cgpadwick@gmail.com>
2024-03-22 16:59:17 -0700

    fix bugs in infra_manager

--- infra_manager.py
+++ infra_manager.py
@@ -1,7 +1,10 @@
 import argparse
+from dotenv import load_dotenv
+import os
 from pathlib import Path
 import yaml
 from google.cloud import pubsub_v1
+from google.cloud import functions_v1
 
 
 def display_menu():
@@ -11,6 +14,96 @@ def display_menu():
     print("2. Teardown infra")
     print("3. Exit")
     print("\n")
+
+
+def get_function_name(function_name, project_id, location):
+    name = f"projects/{project_id}/locations/{location}/functions/{function_name}"
+    return name
+
+
+def func_exists(name):
+    client = functions_v1.CloudFunctionsServiceClient()
+    try:
+        client.get_function(name=name)
+        return True
+    except Exception:
+        return False
+
+
+def delete_cloud_functions_from_yaml(config_file):
+    """
+    Load the Cloud Function configuration from the YAML file
+    Initialize the Cloud Functions client
+    Delete the Cloud Function
+    Print a success message after deleting the Cloud Function
+    """
+    # Load the Cloud Function configuration from the YAML file
+    with open(config_file, "r") as file:
+        data = yaml.safe_load(file)
+
+    # Initialize the Cloud Functions client
+    client = functions_v1.CloudFunctionsServiceClient()
+
+    for function_name, attr in data.get("functions", {}).items():
+
+        name = get_function_name(function_name, data["project_id"], attr["location"])
+        if not func_exists(name):
+            print("no function exists with name: " + function_name)
+            continue
+
+        # Delete the Cloud Function
+        client.delete_function(name=name)
+        print(f"Cloud Function '{function_name}' deleted successfully.")
+
+
+def create_cloud_functions_from_yaml(config_file):
+    """
+    Load the Cloud Function configuration from the YAML file
+    """
+
+    load_dotenv()
+
+    # Load the Cloud Function configuration from the YAML file
+    with open(config_file, "r") as file:
+        data = yaml.safe_load(file)
+
+    # Initialize the Cloud Functions client
+    client = functions_v1.CloudFunctionsServiceClient()
+
+    for function_name, attr in data.get("functions", {}).items():
+
+        name = get_function_name(function_name, data["project_id"], attr["location"])
+
+        if func_exists(name):
+            print("function already exists: " + function_name)
+            continue
+
+        trigger_topic = f"projects/{data['project_id']}/topics/{attr['trigger_topic']}"
+        function = functions_v1.CloudFunction()
+        function.entry_point = attr["entry_point"]
+        function.runtime = attr["runtime"]
+        function.name = name
+        function.max_instances = attr["max_instances"]
+        function.event_trigger = functions_v1.EventTrigger()
+        function.event_trigger.event_type = "google.pubsub.topic.publish"
+        function.event_trigger.resource = trigger_topic
+        function.source_repository = functions_v1.SourceRepository()
+        function.source_repository.url = attr["source_url"]
+
+        envvars = {}
+        for var in attr["env_vars"]:
+            envvars[var] = os.getenv(var)
+        function.environment_variables = envvars
+
+        request = functions_v1.CreateFunctionRequest(
+            location=f"projects/{data['project_id']}/locations/{attr['location']}",
+            function=function,
+        )
+
+        operation = client.create_function(request=request)
+        result = operation.result()
+
+        print(f"Cloud Function '{function_name}' created successfully.")
 
 
 def create_topics_and_subscribers_from_yaml(yaml_file):
@@ -151,7 +244,9 @@ if __name__ == "__main__":
         choice = input("Enter your choice: ")
         if choice == "1":
             create_topics_and_subscribers_from_yaml(args.configfile)
+            create_cloud_functions_from_yaml(args.configfile)
         elif choice == "2":
+            delete_cloud_functions_from_yaml(args.configfile)
             destroy_topics_and_subscribers_from_yaml(args.configfile)
         elif choice == "3":
             print("Exiting")
