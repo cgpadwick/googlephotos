6713ffef2eacc5b3abda37199fc8a2133f1a8105
Chris Padwick <cgpadwick@gmail.com>
2024-04-22 15:17:11 -0700

    add authentication and enforce auth to access image gallery

--- reactui/image-gallery/pages/image-gallery.js
+++ reactui/image-gallery/pages/image-gallery.js
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import SearchInterface from '../components/InstantSearch';
+import {firebaseApp, auth} from '../firebase/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useRouter } from 'next/router';
 import '../styles/styles.css';
 
 export async function fetchSignedUrls(bucketName, fileNames) {
@@ -22,6 +25,48 @@ export async function fetchSignedUrls(bucketName, fileNames) {
 }
 
 function ImageGallery() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const handleError = () => {
+    router.push('/');
+  }
+
+  useEffect(() => {
+    // Check if user is logged in
+    const unsubscribe = onAuthStateChanged(auth, authenticatedUser => {
+      if (authenticatedUser) {
+        // User is signed in.
+        setUser(authenticatedUser);
+      } else {
+        // No user is signed in. Redirect to login page.
+        setError('You must be logged in to access this page.');
+      }
+      setLoading(false);
+    });
+
+    // Clean up the subscription
+    return () => unsubscribe();
+  }, [router]);
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        Checking authentication status... <span className="spinner"></span>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return (
+      <div>
+        {error && <div className="error-message">{error}</div>}
+        <button onClick={handleError}>Go To Login</button>
+      </div>
+    );
+  }
 
   return (
     <div>
