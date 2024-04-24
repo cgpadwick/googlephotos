de4ffd1084b951a992c76804b9ba0d5c9f75281b
Chris Padwick <cgpadwick@gmail.com>
2024-04-24 10:17:54 -0700

    update the populatetestdb script to generate captions

--- util/populatetestdb.py
+++ util/populatetestdb.py
@@ -3,31 +3,21 @@ from pathlib import Path
 import sys
 
 sys.path.insert(0, "../src")
+import time
 import yaml
-
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 
 import photosapp
 
 from ingest_customer_data import ingest_data
+from generate_captions import generate_captions
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--configfile",
-        required=True,
-        type=Path,
-        help="path to the yaml config file",
-    )
 
-    args = parser.parse_args()
+def populate_test_db(config_file, maxmessages=None):
 
     with open(args.configfile, "r") as file:
         config = yaml.safe_load(file)
 
-    db_helper = photosapp.DatabaseHelper(args.configfile, test_mode=True)
+    db_helper = photosapp.DatabaseHelper(config_file, True)
 
     # Start from a clean DB.
     db_helper.clear_firestore_collections()
@@ -42,4 +32,28 @@ if __name__ == "__main__":
     db_helper.insert_customer(customer)
 
     # Add some data to the customer table.
-    ingest_data(args.configfile, customer.email, maxmessages=10, test_mode=True)
+    print("\n\nIngesting Data into the database...")
+    ingest_data(config_file, customer.email, maxmessages=maxmessages, test_mode=True)
+    time.sleep(10)
+    print("Done. \n\n")
+
+    # Generate captions for the images in the test database.
+    print("Generating captions for the images...")
+    generate_captions(
+        config_file, customer.email, maxmessages=maxmessages, test_mode=True
+    )
+    print("Done. \n\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--configfile",
+        required=True,
+        type=Path,
+        help="path to the yaml config file",
+    )
+
+    args = parser.parse_args()
+
+    populate_test_db(args.configfile, maxmessages=None)
