0849ad68e21c1e5286a22582131a7ebb0fb72bee
Chris Padwick <cgpadwick@gmail.com>
2024-04-26 16:31:49 -0700

    made countcaptions.py more general

--- util/countrecordswithfield.py
+++ util/countrecordswithfield.py
@@ -25,6 +25,12 @@ if __name__ == "__main__":
         help="the email to check the captions for",
     )
     parser.add_argument(
+        "--field",
+        required=True,
+        type=str,
+        help="the field that you want to count, e.g. 'caption'",
+    )
+    parser.add_argument(
         "--testmode",
         required=False,
         action="store_true",
@@ -40,12 +46,12 @@ if __name__ == "__main__":
     )
     image_list = col_ref.stream()
 
-    total_num_captions = 0
+    total_num_records = 0
     for img in tqdm(image_list):
-        img_caption = img.to_dict().get("caption", None)
-        if img_caption is not None:
-            total_num_captions += 1
+        field_value = img.to_dict().get(args.field, None)
+        if field_value is not None:
+            total_num_records += 1
 
     print(
-        f"Number of captions found for customer with email {args.email} was : {total_num_captions}"
+        f"Number of records found for customer with email {args.email} with field \'{args.field}\' was : {total_num_records}"
     )
